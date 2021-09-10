8b3b1b205c9184d51be3e3afe5d58a8012cb6674
BBE1210 <39342723+BBE1210@users.noreply.github.com>
2021-09-10 15:43:09 -0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,15 @@
+
+console.log('Assignment 1');
+alert('This progam calculates your age in days.');
+let username = prompt('Please enter your name?');
+let age = prompt('Please enter your age?');
+let gndr = prompt('Please enter your GENDER (M/F)');
+alert(gndr);
+let numdays=365;
+let daysalive=(age*numdays);
+alert(username+', you are '+daysalive+' days old');
+if (gndr === 'M') 
+{alert('The global life expectancy at birth for Men is 70 years or 25550 days');}
+else if (gndr === 'F')
+{alert('The global life expectancy at birth for Women is 75 years or 27375 days');}
+else {alert('You are alive');}
