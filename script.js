3a8c897a241d6f42916d705f2c4697c79df06223
BBE1210 <39342723+BBE1210@users.noreply.github.com>
2021-09-11 07:16:39 -0500

    Add files via upload

--- script.js
+++ script.js
@@ -8,8 +8,8 @@ alert(gndr);
 let numdays=365;
 let daysalive=(age*numdays);
 alert(username+', you are '+daysalive+' days old');
-if (gndr === 'M') 
+if (gndr === 'M' || gndr ==='m') 
 {alert('The global life expectancy at birth for Men is 70 years or 25550 days');}
-else if (gndr === 'F')
+else if (gndr === 'F'|| gndr ==='f' )
 {alert('The global life expectancy at birth for Women is 75 years or 27375 days');}
 else {alert('You are alive');}
